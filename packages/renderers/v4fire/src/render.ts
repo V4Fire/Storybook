fb4c744b382ea64878523caf8c4082d9992ca4da
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-07-05 21:51:20 +0700

    chore(renderer-v4fire): implement render

--- packages/renderers/v4fire/src/render.ts
+++ packages/renderers/v4fire/src/render.ts
@@ -2,25 +2,46 @@
 import type { RenderContext, ArgsStoryFn } from '@storybook/types';
 import type { V4FireRenderer } from './types';
 
-export const render: ArgsStoryFn<V4FireRenderer> = (props, context) => {
-  const { id, component: Component } = context;
-  if (!Component) {
+const isSlotRegex = /^slot:(.*)$/;
+
+export const render: ArgsStoryFn<V4FireRenderer, Dictionary<any>> = (props, context) => {
+  const { id, component: componentOrName } = context;
+  if (!componentOrName) {
     throw new Error(
       `Unable to render story ${id} as the component annotation is missing from the default export`
     );
   }
 
-  // TODO: implement render
-  return '';
+  const {children = {}, ...attrs} = props;
+
+  if (Object.isPlainObject(children)) {
+    Object.entries(attrs).forEach(([key, value]) => {
+      const matches = key.match(isSlotRegex);
+
+      if (matches) {
+        const slotName = matches[1];
+        children[slotName] = value;
+      }
+    });
+  }
+
+  // TODO: in future component may be passed as the class
+  globalThis.renderComponents(
+    componentOrName,
+    [{attrs, children: <RenderComponentsVnodeParams['children']>children}]
+  );
 };
 
 
-export function renderToCanvas(
+export async function renderToCanvas(
   { storyFn, forceRemount, showMain, showException, storyContext }: RenderContext<V4FireRenderer>,
   canvasElement: V4FireRenderer['canvasElement']
 ) {
-  // TODO: implement render to canvas
+  // TODO: handle remounts and re-renders correctly
+  await globalThis.v4fireStorybook.initApp(canvasElement);
 
+  storyFn(); 
+  showMain();
 
   // TODO: return teardown function
   return () => {};
--- packages/renderers/v4fire/src/types.ts
+++ packages/renderers/v4fire/src/types.ts
@@ -1,5 +1,4 @@
 import type { StoryContext as StoryContextBase, WebRenderer } from '@storybook/types';
-import { Constructor } from 'type-fest';
 
 export type { RenderContext } from '@storybook/types';
 
@@ -8,13 +7,13 @@ export interface ShowErrorArgs {
   description: string;
 }
 
-export type StoryFnV4FireReturnType = string | Node;
+export type StoryFnV4FireReturnType = void;
 
 export type StoryContext = StoryContextBase<V4FireRenderer>;
 
 export interface V4FireRenderer extends WebRenderer {
   // FIXME: use ComponentInterface, but it can't be built now due to TS4023, TS2527 errors
   // at @v4fire/client/src/core/component/engines/vue3/render.ts(110,2)
-  component: Constructor<{ $props: any }>;
+  component: string;
   storyResult: StoryFnV4FireReturnType;
 }
