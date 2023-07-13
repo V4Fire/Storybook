67e4fb65e6591dcabe0b9fdcdd6eded750fe8e90
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-07-13 19:36:28 +0700

    chore(framework-webpack5): export basename util

--- packages/frameworks/v4fire-webpack5/src/utils/index.ts
+++ packages/frameworks/v4fire-webpack5/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as layersToRegex } from './layers-to-regex';
 export { default as getStoriesPathsForLayers } from './get-stories-paths-for-layers';
+export { default as basename } from './basename';
