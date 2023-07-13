7839cb00951aa571f0895c73691dae7469cdd79c
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-07-13 19:36:11 +0700

    feat(framework-webpack5): include mdx stories from layers

--- packages/frameworks/v4fire-webpack5/src/utils/get-stories-paths-for-layers.ts
+++ packages/frameworks/v4fire-webpack5/src/utils/get-stories-paths-for-layers.ts
@@ -1,9 +1,10 @@
 /**
  * Returns stories paths for layers
- * 
- * @param {string[]} layers
+ * @param layers
  */
 export default function getStoriesPathsForLayers(layers: string[]): string[] {
-  return layers.map((layer) => `../node_modules/${layer}/src/**/*.stories.@(js|jsx|ts|tsx)`);
+  return layers.map((layer) => `../node_modules/${layer}/src/**/*.mdx`).concat(
+    layers.map((layer) => `../node_modules/${layer}/src/**/*.stories.@(js|ts)`)
+  );
 } 
 
